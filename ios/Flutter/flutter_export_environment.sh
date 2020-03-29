4578782b21be2be4affcad059cc586cf62f1bb4a
jselmani <jselmanovski1@myseneca.ca>
2020-03-29 19:05:33 -0400

    Completed Xylophone app

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/jiel-selmani/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/jiel-selmani/Projects/flutter-practice/xylophone-flutter"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/jiel-selmani/Development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+/*
+* => is used when there is only a single line of code in the function.
+* For example, int add() => 5 + 5; is the equivalent to the function below.
+*
+* int add() {
+*   return 5 + 2;
+* }
+*
+* int add(int n1, int n2) => n1 + n2; is also the same thing.
+*
+* It's nice shorthand syntax to save space and improve readability.
+* */
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
+  void playSound(String filename) {
+    player.play(filename);
+  }
+
+  Expanded buildKey(Color color, String soundFile) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundFile);
+        },
+        child: null,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(Colors.red, 'note1.wav'),
+              buildKey(Colors.orange, 'note2.wav'),
+              buildKey(Colors.yellow, 'note3.wav'),
+              buildKey(Colors.green, 'note4.wav'),
+              buildKey(Colors.green.shade900, 'note5.wav'),
+              buildKey(Colors.blue, 'note6.wav'),
+              buildKey(Colors.purple, 'note7.wav'),
+            ],
+          ),
         ),
       ),
     );
